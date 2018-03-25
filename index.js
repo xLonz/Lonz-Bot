538cdaa88062a0577117f628ff4746c24d3e10dd
xLonz <37774889+xLonz@users.noreply.github.com>
2018-03-26 01:43:06 +0800

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
 
 client.on("ready", () => {
   console.log(`${bot.user.username} is online!`);
-  bot.user.setActivity("@xLonz#4523 for help", {type: "PLAYING"});
+  bot.user.setActivity("Aura Kingdom Mobile", {type: "PLAYING"});
 });
 
 client.login(process.env.BOT_TOKEN);
