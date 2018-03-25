58451d9b1bc8d6f8bee1c8b1bb14964f39ad283f
xLonz <37774889+xLonz@users.noreply.github.com>
2018-03-26 01:54:45 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
+const botconfig = require("./botconfig.json");
 const Discord = require("discord.js");
 const bot = new Discord.Client();
 
 bot.on("ready", async () => {
   console.log(`${bot.user.username} is online!`);
   bot.user.setActivity("Aura Kingdom Mobile", {type: "PLAYING"});
+});
+
+bot.on("message", async message => {
+  if(message.author.bot) return;
+  if(message.channel.type === "dm") return;
+  
+  let prefix = botconfig.prefix;
+  let messageArray = message.content.split(" ");
+  let cmd = messageArray[0];
+  let args =  messageArray.slice(1);
+
+  if(cmd === `${prefix}hello`){
+    return message.channel.send("Hello!");
+  }
+
+  
 });
 
 bot.login(process.env.BOT_TOKEN);
