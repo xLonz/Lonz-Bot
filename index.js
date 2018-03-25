e584a9e4056f1b273c1aa3241ff117382b0401d2
xLonz <37774889+xLonz@users.noreply.github.com>
2018-03-26 01:47:35 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const Discord = require("discord.js");
-const client = new Discord.Client();
+const bot = new Discord.Client();
 
-client.on("ready", async () => {
+bot.on("ready", async () => {
   console.log(`${bot.user.username} is online!`);
-  client.user.setActivity("Aura Kingdom Mobile", {type: "PLAYING"});
+  bot.user.setActivity("Aura Kingdom Mobile", {type: "PLAYING"});
 });
 
-client.login(process.env.BOT_TOKEN);
+bot.login(process.env.BOT_TOKEN);
