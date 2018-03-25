a7348914812a2a237593db8ec5bead771d1f6c69
xLonz <37774889+xLonz@users.noreply.github.com>
2018-03-26 02:02:40 +0800

    Update index.js

--- index.js
+++ index.js
@@ -16,8 +16,8 @@ bot.on("message", async message => {
   let cmd = messageArray[0];
   let args =  messageArray.slice(1);
 
-  if(cmd === `${prefix}hello`){
-    return message.channel.send("Hello!");
+  if(cmd === `${prefix}Hi`){
+    return message.channel.send("Hello! Welcome to UNION :15:");
   }
 
   
