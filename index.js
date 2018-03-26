f2afbf1757cf5bf942108b96bc1f69324951da37
xLonz <37774889+xLonz@users.noreply.github.com>
2018-03-26 16:26:17 +0800

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,17 @@ bot.commands = new Discord.Collection();
 
 fs.readdir("/commands/", (err, files) => {
   
+  if(err) console.log(err);
+  
+  let jsfile = files.filter(f => f.split(".").pop() === "js")
+  if(jsfile.length <=0){
+  }
+  
+  jsfile.forEach((f, i) => {
+    let props = require(`/commands/${f}`);
+    bot.commands.set(props,help.name, props);
+    
+  });
   
 });
 
