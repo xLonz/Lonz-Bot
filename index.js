08755de2df436fc87f6c38e3cf063b79e6911391
xLonz <37774889+xLonz@users.noreply.github.com>
2018-03-26 16:14:25 +0800

    Update index.js

--- index.js
+++ index.js
@@ -3,26 +3,6 @@ const Discord = require("discord.js");
 const bot = new Discord.Client();
 bot.commands = new Discord.Collection();
 
-
-fs.readdir("./commands/", (err, files) => {
-  
-  if(err) console.log(err);
-  
-  let jsfile = files.filter(f => f.split(".").pop() === "js")
-  if(jsfile.length <= 0){
-    console.log("Couldn't find commands.");
-    return;
-  }
-  
-  jsfile.forEach((f, i) => {
-    let props = require(`./commands/${f}`);
-    console.log(`${f} loaded!`);
-    bot.commands.set(props.help.name, props);
-
-  });
-  
-});
-
 bot.on("ready", async () => {
   console.log(`${bot.user.username} is online!`);
   bot.user.setActivity("Aura Kingdom Mobile", {type: "PLAYING"});
