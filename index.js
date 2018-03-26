40f837dd0f23237e4ca9b91b80d73d2675108ebd
xLonz <37774889+xLonz@users.noreply.github.com>
2018-03-26 16:40:36 +0800

    Update index.js

--- index.js
+++ index.js
@@ -4,17 +4,8 @@ const bot = new Discord.Client();
 const fs = require("fs");
 bot.commands = new Discord.Collection();
 
-fs.readdir("/commands/", (err, files) => {
-  
-  let jsfile = files.filter(f => f.split(".").pop() === "js")
-  if (jsfile.length <= 0){
-  }
-  
-  jsfile.forEach((f, i) => {
-    let props = require(`/commands/${f}`);
-    bot.commands.set(props,help.name, props);
-    
-  });
+fs.readdir("../commands/", (err, files) => {
+});'
   
 bot.on("ready", async () => {
   console.log(`${bot.user.username} is online!`);
@@ -78,7 +69,6 @@ bot.on("message", async message => {
   }
    
   
-});
 });
 
 bot.login(process.env.BOT_TOKEN);
