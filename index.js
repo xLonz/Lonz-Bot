4b6b66e0c784f8f967de332e7010bb5b3fd85c16
xLonz <37774889+xLonz@users.noreply.github.com>
2018-03-26 14:49:30 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const botconfig = require("./botconfig.json");
 const Discord = require("discord.js");
 const bot = new Discord.Client();
+let xp = require("./xp.json");
+
 
 bot.on("ready", async () => {
   console.log(`${bot.user.username} is online!`);
