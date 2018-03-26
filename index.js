cfaeec5f963dd385f8fea31c301be8f2596484c4
xLonz <37774889+xLonz@users.noreply.github.com>
2018-03-26 13:10:18 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const botconfig = require("./botconfig.json");
 const Discord = require("discord.js");
-const bot = new Discord.Client();
 const fs = require("fs");
+const bot = new Discord.Client();
 bot.commands = new Discord.Collection();
 
 fs.readdir("./commands/", (err, files) =>{
