db0ecb90efab137d96b49545902d7139a86a71d6
xLonz <37774889+xLonz@users.noreply.github.com>
2018-03-26 17:16:30 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const botconfig = require("./botconfig.json");
 const Discord = require("discord.js");
 const bot = new Discord.Client();
 const url = require("url");
-const superagent = require("superagent");
+var superagent = require('superagent');
 
 bot.on("ready", async () => {
   console.log(`${bot.user.username} is online!`);
