0d4cc4de905fe6a59bfaf353d560290c6a308354
xLonz <37774889+xLonz@users.noreply.github.com>
2018-03-26 12:21:41 +0800

    Update index.js

--- index.js
+++ index.js
@@ -19,6 +19,31 @@ bot.on("message", async message => {
   if(cmd === `${prefix}Hi`){
     return message.channel.send("Hello, Welcome to the official Union Aura Kingdom Mobile Discord! Please enjoy your stay! If you require assistance, please tag @LEADER @DEPUTY @ELITE regarding your questions and concerns.");
   }
+  
+ if(cmd === `${prefix}botinfo`){
+   
+   let (bicon) = bot.user.displayAvatarURL;
+   let botembed = new Discord.RichEmbed()
+   .setDescription("Bot Information")
+   .setColor("#15f153")
+   .setThumbnail(bicon)
+   .addField("Bot Name", bot.user.username);
+   
+   return message.channel.send(botembed);
+ }
+  
+  if(cmd === `${prefix}serverinfo`){
+    
+    let sicon = message.guild.displayAvatarURL;
+    let serverembed = new Discord.RichEmbed()
+    .setDescription("Server Information")
+    .setColor("#15f153")
+    .setThumbnail(sicon)
+    .addField("Server Name", message.guild.name)
+    .addField("Total Members", message.guild.memberCount);
+    
+    return message.channel.send(serverembed);
+  }
 
   
 });
