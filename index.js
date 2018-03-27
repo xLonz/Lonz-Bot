3b5414663ff54738d9ebad032316199679159e16
xLonz <37774889+xLonz@users.noreply.github.com>
2018-03-27 14:09:34 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
 const botconfig = require("./botconfig.json");
 const Discord = require("discord.js");
 const superagent = require("superagent");
+const fs = require ("fs");
 const bot = new Discord.Client();
+bot.commands = new Discord.Collection();
 
+fs.readdir("./commands/", (err, files) = > {
+
+  if(err) console.log(err);
+  
+  let jsfile = files.filter(f => f.split(".").pop() === "js")
+  if(jsfile.length <= 0) {
+    console.log("Couldn't find commands.");
+  return;
+}
+           
+   jsfile.forEach((f, i) => {
+   let props = require(`./commands/${f}`);
+   console.log(`${f} loaded!`);
+   bot.commands.set(props.help.name, props);
+});
+  
+  });
 
 bot.on("ready", async () => {
   console.log(`${bot.user.username} is online!`);
@@ -33,6 +52,9 @@ bot.on("message", async message => {
   let messageArray = message.content.split(" ");
   let cmd = messageArray[0];
   let args =  messageArray.slice(1);
+  
+  let commandfile = bot.commands.get(cmd.slice(prefix.length));
+  if(commandsfile) = commandfile.file(bot.message.args);
   
   if(cmd === `${prefix}ping`){
     return message.channel.send("Hello, Welcome to the official Union Aura Kingdom Mobile Discord! Please enjoy your stay! If you require assistance, please tag @LEADER @DEPUTY @ELITE regarding your questions and concerns.")
