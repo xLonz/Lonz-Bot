c9ae4e061d4ef96966d7fd6b581da38463c02de3
xLonz <37774889+xLonz@users.noreply.github.com>
2018-03-27 14:23:29 +0800

    Update index.js

--- index.js
+++ index.js
@@ -8,18 +8,11 @@ bot.commands = new Discord.Collection();
 fs.readdir("./commands/", (err, files) = > {
 
   if(err) console.log(err);
-  
-  let jsfile = files.filter(f => f.split(".").pop() === "js")
-  if(jsfile.length <= 0) {
-    console.log("Couldn't find commands.");
-  return;
-}
+});       
            
    jsfile.forEach((f, i) => {
    let props = require(`./commands/${f}`);
-   console.log(`${f} loaded!`);
    bot.commands.set(props.help.name, props);
-});
   
   });
 
