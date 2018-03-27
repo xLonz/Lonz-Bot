081161f4f8a37a083c82b81a48e595b070e4cff7
xLonz <37774889+xLonz@users.noreply.github.com>
2018-03-27 17:02:02 +0800

    Update index.js

--- index.js
+++ index.js
@@ -34,93 +34,92 @@ let args =  messageArray.slice(1);
   
 });
              
- // if(cmd === `${prefix}ping`){
-   // return message.channel.send("Hello, Welcome to the official Union Aura Kingdom Mobile Discord! Please enjoy your stay! If you require assistance, please tag @LEADER @DEPUTY @ELITE regarding your questions and concerns.")
- // }
+if(cmd === `${prefix}ping`){
+   return message.channel.send("Hello, Welcome to the official Union Aura Kingdom Mobile Discord! Please enjoy your stay! If you require assistance, please tag @LEADER @DEPUTY @ELITE regarding your questions and concerns.")
+}
  
- //if(cmd === `${prefix}botinfo`){
+ if(cmd === `${prefix}botinfo`){
    
- //  let bicon = bot.user.displayAvatarURL;
-  // let botembed = new Discord.RichEmbed()
-  // .setDescription("Bot Information")
-   //.setColor("#15f153")
-   //.setThumbnail(bicon)
-  // .addField("Bot Name", bot.user.username);
+  let bicon = bot.user.displayAvatarURL;
+  let botembed = new Discord.RichEmbed()
+  .setDescription("Bot Information")
+  .setColor("#15f153")
+  .setThumbnail(bicon)
+  .addField("Bot Name", bot.user.username);
    
-   //return message.channel.send(botembed);
-// }
+  return message.channel.send(botembed);
+}
   
- // if(cmd === `${prefix}serverinfo`){
+ if(cmd === `${prefix}serverinfo`){
     
-   // let sicon = message.guild.iconURL;
-  //  let serverembed = new Discord.RichEmbed()
-    //.setDescription("Server Information")
-   // .setColor("#15f153")
-   // .setThumbnail(sicon)
-  //  .addField("Server Name", message.guild.name)
-  //  .addField("Total Members", message.guild.memberCount);
+  let sicon = message.guild.iconURL;
+  let serverembed = new Discord.RichEmbed()
+  .setDescription("Server Information")
+  .setColor("#15f153")
+  .setThumbnail(sicon)
+  .addField("Server Name", message.guild.name)
+  .addField("Total Members", message.guild.memberCount);
     
- //   return message.channel.send(serverembed);
- // }
+    return message.channel.send(serverembed);
+  }
   
-  //if(cmd === `${prefix}dog`){
+  if(cmd === `${prefix}dog`){
     
- //   let {body} = await superagent
-  //  .get(`https://random.dog/woof.json`);
+    let {body} = await superagent
+    .get(`https://random.dog/woof.json`);
     
- //   let dogembed = new Discord.RichEmbed()
-  //  .setColor("#ff9900")
-  //  .setTitle("Doggo :dog:")
-  //  .setImage(body.url);
+    let dogembed = new Discord.RichEmbed()
+    .setColor("#ff9900")
+    .setTitle("Doggo :dog:")
+    .setImage(body.url);
     
-  //  return message.channel.send(dogembed);
- // }
+    return message.channel.send(dogembed);
+  }
   
- // if(cmd === `${prefix}cat`){
+  if(cmd === `${prefix}cat`){
     
- //   let {body} = await superagent
-  //  .get(`https://random.cat/meow`);
+   let {body} = await superagent
+   .get(`https://random.cat/meow`);
     
- //   let catembed = new Discord.RichEmbed()
-  //  .setColor("#ff9900")
-  //  .setTitle("Cat :cat:")
-  //  .setImage(body.file);
+   let catembed = new Discord.RichEmbed()
+    .setColor("#ff9900")
+    .setTitle("Cat :cat:")
+    .setImage(body.file);
     
-  //  return message.channel.send(catembed);
- // }
+    return message.channel.send(catembed);
+  }
   
-//  if(cmd === `${prefix}avatar`){
+  if(cmd === `${prefix}avatar`){
     
- //   let msg = await message.channel.send("Generating Avatar...");
-  //  let target = message.mentions.users.first() || message.author;
+    let msg = await message.channel.send("Generating Avatar...");
+    let target = message.mentions.users.first() || message.author;
     
- //   await message.channel.send({files: [
-   //                             {
-    //                            attachment: target.displayAvatarURL,
-     //                           name: "avatar.png"
-      //                          }
-        //                        ]});
-   // msg.delete();
-  //}           
-  
-  
-//  if(cmd === `${prefix}present`){
-    
-  //let attendanceEmbed = new Discord.RichEmbed()
- // .setDescription("Attendance")
-  //.setColor("#15f153")
-  //.addField("Member Present", `${message.author}`)
-  //.addField("Time", message.createdAt)
-  
-//  let attendancechannel = message.guild.channels.find(`name`, "union-attendance");
-//  if (!attendancechannel) return message.channel.send("Couldn't find attendance channel.");
+    await message.channel.send({files: [
+                                {
+                                attachment: target.displayAvatarURL,
+                                name: "avatar.png"
+                                }
+                                ]});
+    msg.delete();
+  }           
   
   
-//  message.delete().catch(O_o=>{});
- // attendancechannel.send(attendanceEmbed);
+  if(cmd === `${prefix}present`){
+    
+  let attendanceEmbed = new Discord.RichEmbed()
+  .setDescription("Attendance")
+  .setColor("#15f153")
+  .addField("Member Present", `${message.author}`)
+  .addField("Time", message.createdAt)
   
-//  }
+  let attendancechannel = message.guild.channels.find(`name`, "union-attendance");
+  if (!attendancechannel) return message.channel.send("Couldn't find attendance channel.");
   
+  
+  message.delete().catch(O_o=>{});
+  attendancechannel.send(attendanceEmbed);
+  
+  }
   
 });
 
