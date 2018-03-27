aeafb2806806ad689d94054c1a4b54def49ced7d
xLonz <37774889+xLonz@users.noreply.github.com>
2018-03-27 17:28:53 +0800

    Update index.js

--- index.js
+++ index.js
@@ -32,7 +32,11 @@ if(message.author.bot) return;
 if(message.channel.type === "dm") return;
   
 let prefix = botconfig.prefix;
-
+  if(!message.content.starsWith(prefix)) return;
+  if(cooldown.has(message.author.id)){
+    message.delete();
+    return message.reply("You have to wait 5 seconds between commands.")
+  }
 let messageArray = message.content.split(" ");
 let cmd = messageArray[0];
 let args =  messageArray.slice(1);
